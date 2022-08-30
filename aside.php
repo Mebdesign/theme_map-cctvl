d1718440d52a4125e33259b9fa42f7630953d822
Mebdesign <contact@mebdesign.fr>
2022-08-30 13:38:49 +0200

    add menu

--- aside.php
+++ aside.php
@@ -1,13 +1,29 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-custom-brown" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://ccterresduvaldeloire.fr " target="_blank">
+      <a class="navbar-brand m-0" href="home">
         <img src="<?php echo get_template_directory_uri() . '/assets/img/logos/logocctvl.png'; ?> " class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold text-white">Inventaire équipement</span>
       </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <?php 
+        wp_nav_menu( 
+          array( 
+              'theme_location'  => 'header-menu',
+              'menu_class'      => 'navbar-nav',
+              'container'       => FALSE,
+              'container_id'    => FALSE,
+              'menu_id'         => FALSE,
+              'depth'           => 1,
+              'link_before'     => '<div class="text-white text-center me-2 d-flex align-items-center justify-content-center nav-link-text ms-1">
+              <i class="material-icons opacity-10" style="margin-right:5px;">dashboard</i>' , 
+              'link_after' => '</div> '
+          ) 
+        ); 
+      ?>
+       <!--
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link text-white " href="dashboard">
@@ -33,7 +49,7 @@
             <span class="nav-link-text ms-1">Planning des congés</span>
           </a>
         </li>
-        <!--
+       
         <li class="nav-item">
           <a class="nav-link text-white " href="../pages/virtual-reality.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
