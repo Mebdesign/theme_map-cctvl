cc7a493e096672637638e1d233b58126315c259d
Mebdesign <contact@mebdesign.fr>
2022-08-31 17:43:39 +0200

    add documentation

---
+++ partials/documentation.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Documentation
+*/
+?>
+
+<?php get_header() ?>
+
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+
+    <?php get_template_part('navbar'); ?>
+      <div class="row">
+
+
+        <h6>Documentation</h6>
+      </div>
+
+
+      <?php get_footer() ?>
+
+</main>
+    
+    <?php /*get_template_part('aside-rtl'); */?> 
+    
+    <?php wp_footer(); ?>
+
+</body>
+
+</html>  
