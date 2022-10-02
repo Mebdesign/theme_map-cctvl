a5323c3ef3d744ef54699f09b5c6f1a003bce35b
Mebdesign <contact@mebdesign.fr>
2022-10-02 18:43:24 +0200

    add logout link

--- navbar.php
+++ navbar.php
@@ -17,9 +17,9 @@
           </div>
           <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
-              <a href="../pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
+              <a href="<?php echo wp_logout_url(get_permalink()); ?>" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
+                <span class="d-sm-inline d-none">Déconnexion</span>
               </a>
             </li>
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
