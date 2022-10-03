d468223804d6b799929d09ef0bb85640d60b434b
Mebdesign <contact@mebdesign.fr>
2022-10-03 20:10:25 +0200

    fix login input label

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,13 @@ if (win && document.querySelector('#sidenav-scrollbar')) {
   }
   Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
 }
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var label = document.getElementsByClassName('user_login')
+  var input = document.getElementById('user_login')
+
+  if(input.value !== '') {
+    input.parentElement.classList.add("focused", "is-focused")
+  }
+});
+
