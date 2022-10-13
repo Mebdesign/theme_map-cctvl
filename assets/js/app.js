cd4832149625f71a288f07ac737fa3e7cdd5e279
Mebdesign <contact@mebdesign.fr>
2022-10-13 12:55:51 +0200

    fix show hide password

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 
 var win = navigator.platform.indexOf('Win') > -1;
+
 if (win && document.querySelector('#sidenav-scrollbar')) {
   var options = {
     damping: '0.5'
@@ -8,9 +9,25 @@ if (win && document.querySelector('#sidenav-scrollbar')) {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+  var eye_dashicon = document.querySelector('.wp-pwd .dashicons-visibility')
   var input_user_login = document.getElementById('user_login')
   if(input_user_login?.value !== '') {
     input_user_login?.parentElement?.classList?.add("focused", "is-focused")
   }
+
+  eye_dashicon.addEventListener("click", function(event) { 
+    console.log('alerte')
+    if(this.classList.contains('dashicons-visibility')){
+      this.classList.toggle("dashicons-hidden")
+      this.classList.remove("dashicons-visibility")
+      this.parentElement.previousElementSibling.type = 'text'
+      
+    } else {
+      this.classList.toggle("dashicons-visibility")
+      this.classList.remove("dashicons-hidden")
+      this.parentElement.previousElementSibling.type = 'password'
+    }
+   })
+  
 });
 
