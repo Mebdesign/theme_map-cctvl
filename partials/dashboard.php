8f3dcb20cc04ea3ef1622d767aff8ac9c41f29e5
Grégory MEBARKI <gregory.mebarki@gmail.com>
2022-11-12 11:08:29 +0100

    add lignes en cours d'engagement

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -13,6 +13,7 @@ $copieurs = array();
 $internet = array();
 $phones = array();
 $mobiles = array();
+$engaged_line = array();
 
 if( $posts ):
   foreach( $posts as $post ):
@@ -21,32 +22,31 @@ if( $posts ):
           while ( have_rows('copieurs_et_impressions') ) : the_row();
               $materiel = array_push($copieurs, get_sub_field('materiel'));
           endwhile;
-      else :
-          echo('No copieurs, ');
       endif;
 
       if( have_rows('tel_fixes') ):
         while ( have_rows('tel_fixes') ) : the_row();
             $materiel = array_push($internet, get_sub_field('internet'));
         endwhile;
-      else :
-          echo('No copieurs, ');
       endif;
 
       if( have_rows('tel_mobiles') ):
         while ( have_rows('tel_mobiles') ) : the_row();
-            $materiel = array_push($mobiles, get_sub_field('lignes_mobiles'));
+            array_push($mobiles, get_sub_field('lignes_mobiles'));
+            $fin_dengagement = get_sub_field('fin_dengagement');
+            $new_fin_dengagement = date("d/m/Y", strtotime($fin_dengagement));
+            $fin_dengagement_time   =   strtotime($fin_dengagement);
+            $current_time   =   strtotime(date("Y-m-d"));
+              if($fin_dengagement_time > $current_time) :
+                array_push($engaged_line, get_sub_field('lignes_mobiles'));
+              endif;
         endwhile;
-      else :
-          echo('No copieurs, ');
       endif;
 
       if( have_rows('tel_fixes') ):
         while ( have_rows('tel_fixes') ) : the_row();
             $materiel = array_push($phones, get_sub_field('telephones_fixes'));
         endwhile;
-      else :
-          echo('No copieurs, ');
       endif;
 
   endforeach;
@@ -59,7 +59,7 @@ endif;
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
 
     <?php get_template_part('navbar'); ?>
-      <div class="row">
+      <div class="row mb-4">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
           <div class="card">
             <div class="card-header p-3 pt-2">
@@ -118,13 +118,170 @@ endif;
                 <i style="width:24px;" class="material-icons opacity-10">smartphone</i>
               </div>
               <div class="text-end pt-1">
-                <p class="text-sm mb-0">Nombre de mobiles</p>
+                <p class="text-sm mb-0">Nombre de lignes mobiles</p>
                 <h4 class="mb-0"><?php echo count($mobiles); ?></h4>
               </div>
             </div>
             <hr class="dark horizontal my-0">
             <div class="card-footer p-3">
               <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than yesterday</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row mb-4">
+        <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
+          <div class="card">
+            <div class="card-header pb-0">
+              <div class="row">
+                <div class="col-lg-6 col-7">
+                  <h6>Lignes mobiles en cours d'engagement</h6>
+                  <p class="text-sm mb-0">
+                    <i class="fa fa-check text-info" aria-hidden="true"></i>
+                    <span class="font-weight-bold ms-1"><?php echo count($engaged_line); ?> lignes</span> au total
+                  </p>
+                </div>
+                <div class="col-lg-6 col-5 my-auto text-end">
+                  <div class="dropdown float-lg-end pe-4">
+                    <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="fa fa-ellipsis-v text-secondary"></i>
+                    </a>
+                    <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a></li>
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a></li>
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else here</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Utilisateur</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Ligne</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <?php
+                      if( $posts ):
+                        foreach( $posts as $post ):
+                            setup_postdata( $post );
+                            if( have_rows('tel_mobiles') ):
+                              while ( have_rows('tel_mobiles') ) : the_row();
+                              $mobile = get_sub_field('lignes_mobiles');
+                              $prestataire = get_sub_field('prestataire');
+                              $nom = get_sub_field('nom');
+                              $fin_dengagement = get_sub_field('fin_dengagement');
+                              $new_fin_dengagement = date("d/m/Y", strtotime($fin_dengagement));
+                              $fin_dengagement_time   =   strtotime($fin_dengagement);
+                              $current_time   =   strtotime(date("Y-m-d"));
+                                if($fin_dengagement_time > $current_time) :
+                                  array_push($engaged_line, get_sub_field('lignes_mobiles'));
+                                  ?>
+                                    <tr>
+                                      <td>
+                                        <div class="d-flex px-2 py-1">
+                                          <div class="d-flex flex-column justify-content-center">
+                                            <i class="material-icons text-danger text-gradient me-3">smartphone</i>
+                                          </div>
+                                          <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="mb-0 text-sm"><?php echo($nom); ?></h6>
+                                          </div>
+                                        </div>
+                                      </td>
+                                      <td>
+                                        <span class="text-xs font-weight-bold"> <?php echo($mobile); ?> </span>
+                                      </td>
+                                      <td class="align-middle">
+                                        <span class="text-xs font-weight-bold"> <?php echo($new_fin_dengagement); ?> </span>
+                                      </td>
+                                    </tr>
+                                  <?php
+                                endif;
+                              endwhile;
+                            endif;
+                        endforeach;
+                        wp_reset_postdata();
+                      endif;
+                      ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card h-100">
+            <div class="card-header pb-0">
+              <h6>Orders overview</h6>
+              <p class="text-sm">
+                <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
+                <span class="font-weight-bold">24%</span> this month
+              </p>
+            </div>
+            <div class="card-body p-3">
+              <div class="timeline timeline-one-side">
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-success text-gradient">notifications</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, Design changes</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                  </div>
+                </div>
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-danger text-gradient">code</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">New order #1832412</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
+                  </div>
+                </div>
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-info text-gradient">shopping_cart</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Server payments for April</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
+                  </div>
+                </div>
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-warning text-gradient">credit_card</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">New card added for order #4395133</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">20 DEC 2:20 AM</p>
+                  </div>
+                </div>
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-primary text-gradient">key</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Unlock packages for development</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">18 DEC 4:54 AM</p>
+                  </div>
+                </div>
+                <div class="timeline-block">
+                  <span class="timeline-step">
+                    <i class="material-icons text-dark text-gradient">payments</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">New order #9583120</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">17 DEC</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
