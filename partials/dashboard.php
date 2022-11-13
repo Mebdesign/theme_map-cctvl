fb31a51bb349c65390f1ceb69ba094b22bafa930
Grégory MEBARKI <gregory.mebarki@gmail.com>
2022-11-13 07:21:54 +0100

    add status on lines fixes  line in transert

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -14,23 +14,22 @@ $internet = array();
 $phones = array();
 $mobiles = array();
 $engaged_line = array();
+$engaged_line_fixes = array();
 $line_in_transfer = array();
+$line_in_transfer_fixes = array();
 
 if( $posts ):
   foreach( $posts as $post ):
       setup_postdata( $post );
+
+      // Printers
       if( have_rows('copieurs_et_impressions') ):
           while ( have_rows('copieurs_et_impressions') ) : the_row();
               $materiel = array_push($copieurs, get_sub_field('materiel'));
           endwhile;
       endif;
 
-      if( have_rows('tel_fixes') ):
-        while ( have_rows('tel_fixes') ) : the_row();
-            $materiel = array_push($internet, get_sub_field('internet'));
-        endwhile;
-      endif;
-
+      // Mobiles
       if( have_rows('tel_mobiles') ):
         while ( have_rows('tel_mobiles') ) : the_row();
             array_push($mobiles, get_sub_field('lignes_mobiles'));
@@ -52,9 +51,30 @@ if( $posts ):
         endwhile;
       endif;
 
+      // Internet
       if( have_rows('tel_fixes') ):
         while ( have_rows('tel_fixes') ) : the_row();
-            $materiel = array_push($phones, get_sub_field('telephones_fixes'));
+            $materiel = array_push($internet, get_sub_field('internet'));
+        endwhile;
+      endif;
+
+      // Lignes fixes
+      if( have_rows('tel_fixes') ):
+        while ( have_rows('tel_fixes') ) : the_row();
+            array_push($phones, get_sub_field('telephones_fixes'));
+            $new_fin_dengagement = date("d/m/Y", strtotime($fin_dengagement));
+            $fin_dengagement_time   =   strtotime($fin_dengagement);
+            $current_time   =   strtotime(date("Y-m-d"));
+            $status = get_sub_field('status');
+
+            if($fin_dengagement_time > $current_time) :
+              array_push($engaged_line_fixes, get_sub_field('lignes_fixes'));
+            endif;
+            foreach( $status as $stat ):
+              if( $stat == 'En Porta.' ):
+                array_push($line_in_transfer_fixes, get_sub_field('lignes_fixes'));
+              endif ;
+            endforeach;
         endwhile;
       endif;
 
@@ -68,7 +88,7 @@ endif;
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
 
     <?php get_template_part('navbar'); ?>
-      <div class="row mb-4">
+      <div class="row mb-5">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
           <div class="card">
             <div class="card-header p-3 pt-2">
@@ -139,11 +159,16 @@ endif;
         </div>
       </div>
 
-
-      <div class="row mb-4">
+      <!-- Mobiles -->
+      <div class="row mb-5">
         <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
           <div class="card">
-            <div class="card-header pb-0">
+            <div class="card-header p-3 pt-2">
+              <div class="icon icon-lg icon-shape bg-gradient-info shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                <i style="width:24px;" class="material-icons opacity-10">smartphone</i>
+              </div>
+            </div>
+            <div class="card-header mt-3 pb-0">
               <div class="row">
                 <div class="col-lg-6 col-7">
                   <h6>Lignes mobiles en cours d'engagement</h6>
@@ -244,6 +269,7 @@ endif;
                       setup_postdata( $post );
                       if( have_rows('tel_mobiles') ):
                         while ( have_rows('tel_mobiles') ) : the_row();
+                        $mobile = get_sub_field('lignes_mobiles');
                         $status = get_sub_field('status');
                         $nom = get_sub_field('nom');
                         $transfet_date = get_sub_field('date_de_portabilite');
@@ -256,8 +282,151 @@ endif;
                                 <i class="material-icons text-warning text-gradient">sync</i>
                               </span>
                               <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0"><?php echo($stat); ?>, <?php echo($nom); ?></h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"><?php echo($formatted_transfer_date); ?></p>
+                                <h6 class="text-dark text-sm font-weight-bold mb-0">En cours de transfert, <?php echo($nom); ?></h6>
+                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"><?php echo($formatted_transfer_date); ?> - <?php echo $mobile; ?></p>
+                              </div>
+                            </div>
+                          </div>
+                            <?php
+                          endif ;
+                        endforeach;
+                        endwhile;
+                      endif;
+                  endforeach;
+                  wp_reset_postdata();
+                endif;
+                ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Lignes fixes -->
+      <div class="row mb-4">
+        <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
+          <div class="card">
+            <div class="card-header p-3 pt-2">
+              <div class="icon icon-lg icon-shape bg-gradient-success shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                <i style="width:24px;" class="material-icons opacity-10">phone</i>
+              </div>
+            </div>
+            <div class="card-header mt-3 pb-0">
+              <div class="row">
+                <div class="col-lg-6 col-7">
+                  <h6>Lignes fixes en cours d'engagement</h6>
+                  <p class="text-sm mb-0">
+                    <i class="fa fa-check text-info" aria-hidden="true"></i>
+                    <span class="font-weight-bold ms-1"><?php echo count($phones); ?> lignes</span> au total
+                  </p>
+                </div>
+                <div class="col-lg-6 col-5 my-auto text-end">
+                  <div class="dropdown float-lg-end pe-4">
+                    <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="fa fa-ellipsis-v text-secondary"></i>
+                    </a>
+                    <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a></li>
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a></li>
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else here</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Utilisateur</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Ligne</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <?php
+                      if( $posts ):
+                        foreach( $posts as $post ):
+                            setup_postdata( $post );
+                            if( have_rows('tel_fixes') ):
+                              while ( have_rows('tel_fixes') ) : the_row();
+                              $fixes = get_sub_field('telephones_fixes');
+                              $prestataire = get_sub_field('prestataire');
+                              $contact = get_sub_field('contact');
+                              $fin_dengagement = get_sub_field('fin_dengagement');
+                              $new_fin_dengagement = date("d/m/Y", strtotime($fin_dengagement));
+                              $fin_dengagement_time   =   strtotime($fin_dengagement);
+                              $current_time   =   strtotime(date("Y-m-d"));
+                                if($fin_dengagement_time > $current_time) :
+                                  array_push($engaged_line_fixes, get_sub_field('lignes_fixes'));
+                                  ?>
+                                    <tr>
+                                      <td>
+                                        <div class="d-flex px-2 py-1">
+                                          <div class="d-flex flex-column justify-content-center">
+                                            <i class="material-icons text-danger text-gradient me-3">phone</i>
+                                          </div>
+                                          <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="mb-0 text-sm text-uppercase"><?php echo($contact); ?></h6>
+                                          </div>
+                                        </div>
+                                      </td>
+                                      <td>
+                                        <span class="text-xs font-weight-bold"> <?php echo($fixes); ?> </span>
+                                      </td>
+                                      <td class="align-middle">
+                                        <span class="text-xs font-weight-bold"> <?php echo($new_fin_dengagement); ?> </span>
+                                      </td>
+                                    </tr>
+                                  <?php
+                                endif;
+                              endwhile;
+                            endif;
+                        endforeach;
+                        wp_reset_postdata();
+                      endif;
+                      ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card h-100">
+            <div class="card-header pb-0">
+              <h6>Lignes fixes en cours de portabilité</h6>
+              <p class="text-sm">
+                <i class="fa fa-mobile text-success" aria-hidden="true"></i>
+                <span class="font-weight-bold"><?php echo count($line_in_transfer_fixes); ?></span> lignes en cours...
+              </p>
+            </div>
+            <div class="card-body p-3">
+              <?php
+              ?>
+              <div class="timeline timeline-one-side">
+                <?php
+                if( $posts ):
+                  foreach( $posts as $post ):
+                      setup_postdata( $post );
+                      if( have_rows('tel_fixes') ):
+                        while ( have_rows('tel_fixes') ) : the_row();
+                        $fixes = get_sub_field('telephones_fixes');
+                        $status = get_sub_field('status');
+                        $contact = get_sub_field('contact');
+                        $transfet_date = get_sub_field('date_de_portabilite');
+                        $formatted_transfer_date = date("d/m/Y", strtotime($transfet_date));
+                        foreach( $status as $stat ):
+                          if( $stat == 'En Porta.' ): ?>
+                          <div class="timeline timeline-one-side">
+                            <div class="timeline-block mb-3">
+                              <span class="timeline-step">
+                                <i class="material-icons text-warning text-gradient">sync</i>
+                              </span>
+                              <div class="timeline-content">
+                                <h6 class="text-dark text-sm font-weight-bold mb-0">En cours de transfert, <?php echo($contact); ?></h6>
+                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"><?php echo($formatted_transfer_date); ?> - <?php echo $fixes; ?></p>
                               </div>
                             </div>
                           </div>
